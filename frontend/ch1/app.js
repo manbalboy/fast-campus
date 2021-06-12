71f280976bd7107eb45de82421b869877ff11640
manbalboy <manbalboy@hanmail.net>
2021-06-12 22:16:20 +0900

    템플릿 방식 **

--- frontend/ch1/app.js
+++ frontend/ch1/app.js
@@ -3,6 +3,7 @@ let ajax = new XMLHttpRequest();
 const rootEl = document.getElementById('root');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const PAGE_COUNT = 10;
 
 const store = {
     currentPage: 1,
@@ -30,9 +31,21 @@ const newsDetails = function () {
 const newsFeedFn = function () {
     const newsFeed = ajaxFn('GET', NEWS_URL);
     const newsList = [];
-    newsList.push('<ul>');
 
-    for (let i = (store.currentPage - 1) * 10; i < store.currentPage  * 10; i++) {
+    let template = `
+        <div>
+            <h1>Hacker News</h1>
+            <ul>
+                {{__news_feed__}}
+            </ul>
+            <div>
+                <a href="#/page/{{__prev_page__}}">이전페이지</a>
+                <a href="#/page/{{__next_page__}}">다음페이지</a>
+            </div>
+        </div>
+    `;
+
+    for (let i = (store.currentPage - 1) * PAGE_COUNT; i < store.currentPage * PAGE_COUNT; i++) {
         newsList.push(`
             <li>
                 <a href="#/show/${newsFeed[i].id}">
@@ -42,17 +55,11 @@ const newsFeedFn = function () {
         `)
     }
 
-    newsList.push('</ul>');
+    template = template.replace('{{__news_feed__}}', newsList.join(""));
+    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+    template = template.replace('{{__next_page__}}', store.currentPage < Math.ceil(newsFeed.length / PAGE_COUNT) ? store.currentPage + 1 : store.currentPage);
 
-    newsList.push(`
-    
-        <div>
-            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전페이지</a>
-            <a href="#/page/${store.currentPage + 1}">다음페이지</a>
-        </div>
-    `)
-
-    rootEl.innerHTML = newsList.join("");
+    rootEl.innerHTML = template;
 }
 
 const router = function () {
