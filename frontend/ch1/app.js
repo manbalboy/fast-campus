0778b82d0a479d68911859a9ac02565529c053c7
manbalboy <manbalboy@hanmail.net>
2021-06-12 21:29:31 +0900

    라우터 구현

--- frontend/ch1/app.js
+++ frontend/ch1/app.js
@@ -4,7 +4,7 @@ const rootEl = document.getElementById('root');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-const ajaxFn = function (method = 'GET', url, syncFg = false ) {
+const ajaxFn = function (method = 'GET', url, syncFg = false) {
     ajax.open(method, url, syncFg);
     ajax.send();
 
@@ -12,37 +12,50 @@ const ajaxFn = function (method = 'GET', url, syncFg = false ) {
 }
 
 
-const newsFeed = ajaxFn('GET', NEWS_URL);
 
-const ulEl = document.createElement('ul');
 
-const content = document.createElement('div');
 
-window.addEventListener('hashchange', function(e) {
+const newsDetails = function () {
     const id = location.hash.substr(1);
+    const newsContent = ajaxFn('GET', CONTENT_URL.replace('@id', id));
 
-    const newsContent = ajaxFn('GET', CONTENT_URL.replace('@id', id)); 
-    const h2El = document.createElement('h1');
+    rootEl.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
 
-    console.dir(newsContent);
-    h2El.innerHTML = newsContent.title;
 
-    content.appendChild(h2El);
-})
+const newsFeedFn = function () {
+    const newsFeed = ajaxFn('GET', NEWS_URL);
+    const newsList = [];
+    newsList.push('<ul>');
 
-newsFeed.forEach((item) => {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
+    newsFeed.forEach((item) => {
+        newsList.push(`
         <li>
             <a href="#${item.id}">
                 ${item.title} (${item.comments_count})
             </a>
         </li>
-    `;
+    `);
+    });
 
-    ulEl.appendChild(div.children[0]);
-});
+    newsList.push('</ul>');
 
-rootEl.appendChild(ulEl);
-rootEl.appendChild(content);
+    rootEl.innerHTML = newsList.join("");
+}
+
+const router = function () {
+    const routePath = location.hash;
+    if(routePath === '') {
+        newsFeedFn();
+    } else {
+        newsDetails();
+    }
+};
+
+window.addEventListener('hashchange', router);
+router();
