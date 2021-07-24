8cd7382c0f0d1047f5d5d2505234d3b5af3f81ba
manbalboy <manbalboy@hanmail.net>
2021-07-25 00:32:21 +0900

    타입스크립트 getData 함수를 클래스로 변환

--- frontend/ch2/app.ts
+++ frontend/ch2/app.ts
@@ -38,12 +38,35 @@ const store: Store = {
     feeds: [],
 };
 
-function getData<T>(url: string): T {
-    ajax.open('GET', url, false);
-    ajax.send();
+class Api {
+    url: string;
+    ajax : XMLHttpRequest;
 
-    return JSON.parse(ajax.response);
+    constructor(url: string) {
+        this.url = url;
+        this.ajax = new XMLHttpRequest();
+    }
+
+    protected getRequest <AjaxResponse> () : AjaxResponse{
+        this.ajax.open('GET', this.url, false);
+        this.ajax.send();
+
+        return JSON.parse(this.ajax.response);
+    }
 }
+
+class NewsFeedApi extends Api {
+    getData(): NewsFeed[] {
+        return this.getRequest<NewsFeed[]>()
+    }
+}
+
+class NewsDetailApi extends Api {
+    getData(): NewsDetail {
+        return this.getRequest<NewsDetail>();
+    }
+}
+
 
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
     for (let i = 0; i < feeds.length; i++) {
@@ -63,6 +86,7 @@ function updateView(html: string): void {
 
 
 function newsFeed(): void {
+    const api = new NewsFeedApi(NEWS_URL);
     let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
     let template = `
@@ -91,7 +115,7 @@ function newsFeed(): void {
   `;
 
     if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+        newsFeed = store.feeds = makeFeeds(api.getData());
     }
 
     for (let i = (store.currentPage - 1) * PAGE_COUNT; i < store.currentPage * PAGE_COUNT; i++) {
@@ -123,8 +147,11 @@ function newsFeed(): void {
 }
 
 function newsDetail(): void {
+
     const id = location.hash.substr(7);
-    const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id))
+    const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
+    const newsContent = api.getData();
+
     let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -160,7 +187,6 @@ function newsDetail(): void {
             break;
         }
     }
-
 
     updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
 }
